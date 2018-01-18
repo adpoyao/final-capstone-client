b6efb2a7e6a7e9340e23280ee60a6320e24931f9
Nate Garrett <nate.garrett1992@gmail.com>
2018-01-18 16:32:43 -0600

    finished adding actions

---
+++ src/actions/classes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import { FETCH_CLASSES,
+FETCH_CLASSES_SUCCESS,
+FETCH_CLASSES_REQUEST,
+FETCH_CLASSES_ERROR, } from './actionType';
+
+export const fetchClassesSuccess = (classes) => ({
+  type: FETCH_CLASSES_SUCCESS,
+  classes,
+});
+
+export const fetchClassesRequest = () => ({
+  type: FETCH_CLASSES_REQUEST,
+});
+
+export const fetchClassesError = () => ({
+  type: FETCH_CLASSES_ERROR,
+});
+
+export const fetchClasses = username => (dispatch, getState) => {
+  dispatch(fetchClassesRequest());
+  return fetch('http://localhost:3000/api/classes')
+  .then(res => res.json())
+  .then(classes => dispatch(fetchClassesSuccess(classes)));
+};
