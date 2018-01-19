7eaf99e5059a87cfd0e225b0358d617b91bc89ae
Eddie Yao <adpoyao@gmail.com>
2018-01-19 11:11:00 -0800

    Adds tests to class-reducers.

--- src/reducers/classes.js
+++ src/reducers/classes.js
@@ -1,31 +1,31 @@
-import * as types from '../actions/actionType';
+// import * as types from '../actions/actionType';
 
-const initialState = {
-  classes: [],
-  loading: false,
-  error: false,
-};
+// const initialState = {
+//   classes: [],
+//   loading: false,
+//   error: false,
+// };
 
-const classesReducer = (state = initialState, action) => {
-  switch (action.type){
-    case types.FETCH_CLASSES_SUCCESS:
-      return Object.assign({}, state, {
-        classes: action.classes,
-        loading: false,
-        error: false,
-      });
-    case types.FETCH_CLASSES_REQUEST:
-      return Object.assign({}, state, {
-        loading: true,
-        error: false,
-      });
-    case types.FETCH_CLASSES_ERROR:
-      return Object.assign({}, state, {
-        loading: false,
-        error: action.err,
-      });
-  }
-  return state;
-};
+// const classesReducer = (state = initialState, action) => {
+//   switch (action.type){
+//     case types.FETCH_CLASSES_SUCCESS:
+//       return Object.assign({}, state, {
+//         classes: action.classes,
+//         loading: false,
+//         error: false,
+//       });
+//     case types.FETCH_CLASSES_REQUEST:
+//       return Object.assign({}, state, {
+//         loading: true,
+//         error: false,
+//       });
+//     case types.FETCH_CLASSES_ERROR:
+//       return Object.assign({}, state, {
+//         loading: false,
+//         error: action.err,
+//       });
+//   }
+//   return state;
+// };
 
-export default classesReducer;
+// export default classesReducer;
--- src/test/reducers/classes.test.js
+++ src/test/reducers/classes.test.js
@@ -13,15 +13,43 @@ describe('classesReducer', () => {
     );
   });
 
-  it('should handle fetchClasses()', () => {
+  it('should handle fetchClassesSuccess()', () => {
     expect(
-      classesReducer(undefined, {
-        type: types.FETCH_CLASSES,
+      classesReducer({}, {
+        type: types.FETCH_CLASSES_SUCCESS,
         classes: [{ class: 'Run the classes tests' }],
       })
     ).toEqual(
       {
         classes: [{ class: 'Run the classes tests' }],
+        error: false,
+        loading: false
+      }
+    );
+  });
+
+  it('should handle fetchClassesError()', () => {
+    expect(
+      classesReducer({}, {
+        type: types.FETCH_CLASSES_ERROR,
+      })
+    ).toEqual(
+      {
+        error: undefined,
+        loading: false
+      }
+    );
+  });
+
+  it('should handle fetchClassesRequest()', () => {
+    expect(
+      classesReducer({}, {
+        type: types.FETCH_CLASSES_REQUEST,
+      })
+    ).toEqual(
+      {
+        error: false,
+        loading: true
       }
     );
   });
