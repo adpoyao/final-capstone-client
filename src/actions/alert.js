fdefe6aa39b59d8ce2534117eddc03e2aff89325
Nate Garrett <nate.garrett1992@gmail.com>
2018-01-25 15:21:20 -0600

    adds actions and reducers for alert endpoint

---
+++ src/actions/alert.js
@@ -0,0 +1,137 @@
+import * as types from './actionType';
+import { normalizeResponseErrors } from './utils';
+
+//----- SYNC ACTIONS -----//
+
+// Request all alerts
+export const fetchAlertsRequest = () => ({
+  type: types.SEARCH_ALERTS_REQUEST,
+});
+
+// Request Student Alerts
+export const fetchAlertsByStudentRequest = () => ({
+  type: types.FETCH_ALERTS_BY_STUDENT_REQUEST,
+});
+
+// Request Teacher Alerts
+export const fetchAlertsByTeacherRequest = () => ({
+  type: types.FETCH_ALERTS_BY_TEACHER_REQUEST,
+});
+
+// All Alerts Successfully Retrieved
+export const fetchAlertsSuccess = (alerts) => ({
+  type: types.SEARCH_ALERTS_SUCCESS,
+  alerts,
+});
+
+// Student Alerts Successfully Retrieved
+export const fetchAlertsByStudentSuccess = (alerts) => ({
+  type: types.FETCH_ALERTS_BY_STUDENT_SUCCESS,
+  alerts,
+});
+
+// Teacher Alerts Successfully Retrieved
+export const fetchAlertsByTeacherSuccess = (alerts) => ({
+  type: types.FETCH_ALERTS_BY_TEACHER_SUCCESS,
+  alerts,
+});
+
+// UNIVERSAL: Toggle Error
+export const fetchAlertsError = (err) => ({
+  type: types.FETCH_ALERTS_ERROR,
+  err,
+});
+
+//----- STUDENT: ASYNC ACTIONS  -----//
+
+// STUDENT: toggle panic alert on
+export const toggleAlertOn = (studentID) => (dispatch, getState) => {
+  dispatch(fetchAlertsRequest());
+  return fetch('http://localhost:8080/api/alert/panic/on', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        body: JSON.stringify(studentID),
+      })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(() => dispatch(fetchAlertsByStudentSuccess(studentID.panicID)))
+    .then(() => dispatch(fetchAlertsSuccess()))
+    .catch((err) => {
+      dispatch(fetchAlertsError(err));
+    });
+};
+
+// STUDENT: toggle panic alert off
+export const toggleAlertOff = (panicID) => (dispatch, getState) => {
+  return fetch(`http://localhost:8080/api/alert/panic/off`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        body: JSON.stringify(data),
+      })
+     .then(res => normalizeResponseErrors(res))
+     .then(res => res.json())
+     .then(classes => dispatch(fetchUntoggleAlertsSuccess(studentID.panicID)))
+     .catch((err) => {
+      dispatch(fetchAlertsError(err));
+    });
+};
+
+//----- TEACHER: ASYNC ACTIONS  -----//
+
+// TEACHER: Retrieves all critical emotion alerts attached to teacher ID
+export const fetchMoodAlertsByTeacher = teacherID => (dispatch, getState) => {
+  dispatch(fetchAlertsRequest());
+  return fetch(`http://localhost:8080/api/alert/mood/${teacherID}`)
+  .then(res => res.json())
+  .then(alerts => dispatch(fetchAlertsSuccess(alerts)))
+  .catch((err) => {
+    dispatch(fetchAlertsError(err));
+  });
+};
+
+// TEACHER: Retrieves all panic alerts attached to teacher ID
+export const fetchPanicAlertsByTeacher = teacherID => (dispatch, getState) => {
+  dispatch(fetchAlertsRequest());
+  return fetch(`http://localhost:8080/api/alert/panic/${teacherID}`)
+  .then(res => res.json())
+  .then(alerts => dispatch(fetchAlertsSuccess(alerts)))
+  .catch((err) => {
+    dispatch(fetchAlertsError(err));
+  });
+};
+
+// TEACHER: Retrieves all critical moods of a student
+export const fetchCriticalMoodsByStudent = studentID => (dispatch, getState) => {
+  dispatch(fetchAlertsByStudentRequest());
+  return fetch(`http://localhost:8080/api/alert/panic/${studentID}`)
+  .then(res => res.json())
+  .then(alerts => dispatch(fetchAlertsSuccess(alerts)))
+  .catch((err) => {
+    dispatch(fetchAlertsError(err));
+  });
+};
+
+// TEACHER: Dismisses a panic alert
+export const dismissAlert = (data) => (dispatch, getState) => {
+  dispatch(fetchAlertsRequest());
+  return fetch('http://localhost:8080/api/alert/panic/dismiss', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        body: JSON.stringify(data),
+      })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then((res) => dispatch(fetchPanicAlertsByTeacher(res)))
+    .catch((err) => {
+      dispatch(fetchAlertsError(err));
+    });
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,19 @@
 import { combineReducers } from 'redux';
-import {reducer as formReducer} from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
 
 import classesReducer from './classes';
 import viewReducer from './views';
 import authReducer from './auth';
 import protectedDataReducer from './protected-data';
+import alertReducer from './alert';
 
 const rootReducer = combineReducers({
   classes: classesReducer,
   form: formReducer,
   view: viewReducer,
   auth: authReducer,
-  protectedData: protectedDataReducer
+  alert: alertReducer,
+  protectedData: protectedDataReducer,
 });
 
 export default rootReducer;
