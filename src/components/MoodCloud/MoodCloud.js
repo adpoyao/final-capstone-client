34e01abe1f18a1134169e1ad73fc25c645d0c5ed
Adam Bradbury <f.adambrad@gmail.com>
2018-01-26 16:00:46 -0700

    edits

--- src/components/MoodCloud/MoodCloud.js
+++ src/components/MoodCloud/MoodCloud.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { render } from 'react-dom';
+import WordCloud from 'react-d3-cloud';
 import MoodForm from '../Dashboard-Student/MoodForm';
 import MoodCaptions from '../Dashboard-Student/MoodCaptions';
 import MoodSelections from '../Dashboard-Student/MoodSelections';
@@ -11,7 +13,6 @@ export class MoodCloud extends Component {
     console.log('this.props.studentId', this.props.studentId)
 
   }
- 
 
   render() {
     
@@ -30,12 +31,21 @@ export class MoodCloud extends Component {
       //   </div>
       // )
     // }
+    const data = [
+      { text: 'Hey', value: 1000 },
+      { text: 'lol', value: 200 },
+      { text: 'first impression', value: 800 },
+      { text: 'very cool', value: 1000000 },
+      { text: 'duck', value: 10 },
+    ];
 
-
+    const fontSizeMapper = word => Math.log2(word.value) * 5;
+    const rotate = word => word.value % 360;
+  
     return(
       <div className='moodcloud-container'>
         <h1>MoodCLoud</h1>
-        <p>D3 goes here</p>
+        <p><WordCloud data={data} fontSizeMapper={fontSizeMapper} rotate={rotate} /></p>
       </div>
     )
   }
