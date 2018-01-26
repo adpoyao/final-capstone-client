8aec9f5d94c3c83b068570da7224090a1ef62f42
Quang Nguyen <qtn2@njit.edu>
2018-01-26 13:12:50 -0500

    added POST end points

--- src/actions/moods.js
+++ src/actions/moods.js
@@ -1,8 +1,37 @@
 import * as types from './actionType';
+import { normalizeResponseErrors } from './utils';
+import {SubmissionError} from 'redux-form';
 
-// SYNC ACTIONS
+// MOOD: SYNC ACTIONS
 
 export const updateMoodCaption = (mood) => ({
     type: types.UPDATE_MOOD_CAPTION,
     mood
 })
+
+
+
+//----- MOOD: ASYNC ACTIONS  -----//
+export const saveMood = (mood) => (dispatch, getState) => {
+    return fetch('http://localhost:8080/api/mood', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json', 
+              'Accept': 'application/json',
+            },
+          body: JSON.stringify(mood),
+        })
+      .then(res => normalizeResponseErrors(res))
+      .then(res => res.status(200))
+      .catch(err => {
+        const {reason, message, location} = err;
+        if (reason === 'ValidationError') {
+            // Convert ValidationErrors into SubmissionErrors for Redux Form
+            return Promise.reject(
+                new SubmissionError({
+                    [location]: message
+                })
+            );
+        }
+    });
+  };
