11f360ae14e65bb9ace55c4ba37b4cfc7cb2a109
Eddie Yao <adpoyao@gmail.com>
2018-01-29 10:56:53 -0800

    Adds Moodcloud component.

--- src/reducers/moods.js
+++ src/reducers/moods.js
@@ -1,13 +1,31 @@
 import * as types from '../actions/actionType';
 
 const initialState = {
-    selectedMood: ''
+    selectedMood: '',
+    studentMoods: '',
+    loading: false,
+    error: false,
 }
 const moodsReducer = (state=initialState, action) => {
     switch (action.type) {
         case types.UPDATE_MOOD_CAPTION:
             return Object.assign({}, state, {
                 selectedMood: action.mood
+            })
+        case types.FETCH_MOOD_REQUEST:
+            return Object.assign({}, state, {
+                loading: true,
+                error: false
+            })
+        case types.FETCH_MOOD_ERROR:
+            return Object.assign({}, state, {
+                loading: false,
+                error: action.err
+            })
+        case types.FETCH_MOOD_SUCCESS:
+            return Object.assign({}, state, {
+                loading: false,
+                studentMoods: action.moods
             })
         default: break;
     }
