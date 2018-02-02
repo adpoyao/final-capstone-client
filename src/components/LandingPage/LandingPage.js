39d596b77f19c7f183a0f158955d89a5b35114ed
Eddie Yao <adpoyao@gmail.com>
2018-02-01 18:16:20 -0800

    Adds CSS and feature images.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -28,8 +28,8 @@ export class LandingPage extends Component {
       <div className="landing-page-container">
         <div className="landing-page-midsection">
           <div className="landing-page-left">
-            <h3 className='main-tagline'>Bring teachers and students closer together with Mood Cloud</h3>
-            <p>Mood Cloud helps students track their moods over time to develop their Emotional Intelligence(EI). Additionally, Mood Cloud assists teachers by staying up-to-date on their students' emotional well-being, providing students a way to reach out during times of crisis or despair.</p>
+            <h3 className='main-tagline'>Bringing teachers and students closer together.</h3>
+            <p className='sub-tagline'>With Mood Cloud, students can track their moods over time to develop their Emotional Intelligence (EI). Teachers now have a bigger window of opportunity to stay up-to-date on their students' emotional well-being, providing students a way to reach out during times of crisis or despair.</p>
           </div>
           <div className="landing-page-right">
             <img src="https://blogsimages.adobe.com/creativecloud/files/2015/06/ColorExplosion_Header-e1435350239810.jpg" alt="rainbow-fart" />
